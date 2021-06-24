17d1ab4559d532954c612366d9450461d00767f6
RCB500X <ruben.cidb@icloud.com>
2021-06-24 01:39:42 -0400

    finishing

--- index.py
+++ index.py
@@ -1,39 +1,93 @@
-from zoo import Zoo,Animal,Food,Marines,Earthlys,Aerials
+from zoo import Zoo,Animal, Food, Foods, Water, Land, Sky
 
+#Variable Zoologico
 zoo = Zoo('Zoologilandia')
+
+#Variable Comida
+food = Food('food')
+
+#Variable Comida
+zanahoria = Foods('Zanahoria', 10, 10)
+pescado = Foods('Pescado', 20, 10)
+parrillada = Foods('Parrillada', 30, 10)
+papasfritas = Foods('Papas Fritas', -30,20)
+plastico = Foods('Bolsa Plástica', -30, -30)
+superocho = Foods('Super Ocho', -10,10)
+
+#Integración comidas
+food.add_food(zanahoria)
+food.add_food(pescado)
+food.add_food(parrillada)
+food.add_food(papasfritas)
+food.add_food(plastico)
+food.add_food(superocho)
 
 def Main():
     
     print(f"\nWelcome to {zoo.name}")
 
     while True:
-        result = input ('\nWhat do yo want to do today?\n1. Create an Aerial\n2. Create an Earthly\n3. Create an Marine\n4. Feed some animal\n5. Show animals in zoo\n6. Go away\n')
+        result = input ('\nWhat do yo want to do?\n\n1. Create an Water Animal\n2. Create an Land Animal\n3. Create an Sky Animal\n4. Feed some Animal\n5. Do some exercise\n6. Show animals in zoo\n7. Go away\n')
         
         if result == "1":
-            raza = input('¿Qué tipo de animal es?: ')
-            nick = input('Póngale nombre al animal: ')
-            size = input('¿Cuántos cms mide?: ')
-            age = input('¿Qué edad tiene?: ')
-            ave = Aerials(raza,nick,size,age,50,50)
-            print('Creando Ave ... un momento')
-            zoo.add_animal(ave)
-            print('Agregando al zoológico\n')
-            print(f"{ave.nickname} is now part of {zoo.name}!")
+            name = input('\nWhat kind of animal is?: ')
+            nickname = input('what is the name?: ')
+            size = input('How many cms of lenght is this animal?: ')
+            age = input('How old is?: ')
+            zoo.add_water(name, nickname, size, age)
+            print(f"\nCongratulations! {nickname} is now at the zoo.\nWhat would you do now?:")
+            continue
         
-        if result == "2":
-            print('We still working in this space')
-            break
-        if result == "3":
-            print('We still working in this space')
-            break
-        if result == "4":
-            print('We still working in this space')
-            break
-        if result == "5":
-            print('We still working in this space')
-            break
-        if result == "6":
-            print('¡See you later!')
+        elif result == "2":
+            name = input('\nWhat kind of animal is?: ')
+            nickname = input('what is the name?: ')
+            size = input('How many cms of lenght is this animal?: ')
+            age = input('How old is?: ')
+            zoo.add_land(name, nickname, size, age)
+            print(f"\nCongratulations! {nickname} is now at the zoo.\nWhat would you do now?:")
+            continue
+        elif result == "3":
+            name = input('\nWhat kind of animal is?: ')
+            nickname = input('what is the name?: ')
+            size = input('How many cms of lenght is this animal?: ')
+            age = input('How old is?: ')
+            zoo.add_sky(name, nickname, size, age)
+            print(f"\nCongratulations! {nickname} is now at the zoo.\nWhat would you do now?:")
+            continue
+        elif result == "4":
+            print('\nWhat animal do you want to feed?: ')
+            for i in range (0, len(zoo.animals)):
+                print(i, '- ',zoo.animals[i].nickname)
+            choice = int(input('Please write the number of the animal here: '))
+            print(f"\nYou choose {zoo.animals[choice]}")
+            print(f"\nChoose the food you want to give to {zoo.animals[choice].nickname}: ")
+            for i in range (0,len(food.foods)):
+                print(i, '- ', food.foods[i])
+            select = int(input('\nPlease write the number of the option: '))
+            animal = zoo.animals[choice]
+            foody = animal.feed(food.foods[select])
+            print(foody)
+            continue
+        elif result == "5":
+            print('\nWhat animal do you want to exercise?: ')
+            for i in range (0, len(zoo.animals)):
+                print(i, '- ',zoo.animals[i].nickname)
+            choice = int(input('Please write the number of the animal here: '))
+            print(f"\nYou choose {zoo.animals[choice].nickname} the {zoo.animals[choice].name}")
+            distance = int(input(f"\nHow many kilometers you wanna try with {zoo.animals[choice].nickname}?: "))
+            animal = zoo.animals[choice]
+            exercise = animal.exercise(distance)
+            print(exercise)
+            continue
+        elif result == "6":
+            print('\n', zoo.zoo_information())
+            continue
+        elif result == "7":
+            print('\n¡See you later!\n')
             exit()
+        else:
+            print('Please write a valid answer')
+            continue
+
 Main()
 import pdb; pdb.set_trace()
--- zoo.py
+++ zoo.py
@@ -7,9 +7,24 @@ class Zoo: #working
         self.animals.append(animal)
         return self
     
-    def zoo_information(self):#notworkingwell
-        for animal in self.animals:
-            return f"Type: {animal.name}\n Name:{animal.nickname}\n Age: {animal.age}\n"
+    def zoo_information(self):
+        print(f"\n The {self.name}`s animals are: \n")
+        for i in range (len(self.animals)):
+            print(i, '= ', self.animals[i])
+        return (f"As you can count, there are {len(self.animals)} animals in the Zoo today. ¡Have fun!")
+    
+    def add_water(self, name, nickname, size, age):
+        newwater = Water(name, nickname, size, age)
+        self.animals.append(newwater)
+    
+    def add_land(self, name, nickname, size, age):
+        newland = Land(name, nickname, size, age)
+        self.animals.append(newland)
+        
+    def add_sky(self, name, nickname, size, age):
+        newsky = Sky(name, nickname, size, age)
+        self.animals.append(newsky)
+        
 
 class Animal: #working
     
@@ -24,41 +39,57 @@ class Animal: #working
     def display_info(self): #working
         return (f"This {self.name} called {self.nickname} have an {self.health}% of healthy, and is {self.happyness}% happy at the moment")
     
+    def __repr__(self):
+        return f"{self.nickname} the {self.name} has {self.health}% of health and {self.happyness}% of happynes"
+    
+    def exercise(self):
+        raise NotImplementedError
+    
     def feed(self, food): #working
         self.health += food.healthQ
         if self.health >= 100:
             self.health = 100
-            return (f"You gave an {food.name} to {self.nickname}. Congrats! {self.nickname} have 100% of health!")
+            print(f"You gave an {food.name} to {self.nickname}. Congrats! {self.nickname} have 100% of health!")
         elif self.health <= 0:
             self.health = 0
-            return (f"You gave an {food.name} to {self.nickname}. What are you doing!!?? {self.nickname} is critical!!!")
+            print(f"You gave an {food.name} to {self.nickname}. What are you doing!!?? {self.nickname} is critical!!!")
         elif self.health <50:
-            return (f"You gave an {food.name} to {self.nickname}. {self.nickname} the {self.name} have {self.health}% of health, wich is not so good")
+            print(f"You gave an {food.name} to {self.nickname}. {self.nickname} the {self.name} have {self.health}% of health, wich is not so good")
         elif self.health >50:
-            return (f"You gave an {food.name} to {self.nickname}. {self.nickname} have {self.health}% of health, wich is quiet good")
+            print(f"You gave an {food.name} to {self.nickname}. {self.nickname} have {self.health}% of health, wich is quiet good")
         self.happyness += food.happyQ
         if self.happyness >= 100:
             self.happyness = 100
-            return (f"You gave an {food.name} to {self.nickname}. Congrats! {self.nickname} have 100% of hapyness!")
+            print(f"Also, {self.nickname} have 100% of hapyness!")
         elif self.happyness <= 0:
             self.happyness = 0
-            return (f"You gave an {food.name} to {self.nickname}. What are you doing!!?? {self.nickname} have depression and start trying drugs!!")
+            print(f"Also, {self.nickname} have {self.happyness}% of happyness, so fall in depression and start trying drugs!!")
         elif self.happyness <50:
-                return (f"You gave an {food.name} to {self.nickname}. {self.nickname} have {self.happyness}% of health, wich is not so good")
+            print(f"Also, {self.nickname} have {self.happyness}% of health, wich is not so good, take care!")
         elif self.happyness >50:
-            return (f"You gave an {food.name} to {self.nickname}. {self.nickname} have {self.happyness}% of health, wich is quiet good")
-    
-    def exercise(self):
-        raise NotImplementedError
+            print(f"Also, {self.nickname} have {self.happyness}% of health, wich is quiet good")
+        return('Eat is good, but eat too much could be a problem')
 
 class Food: #working
-    def __init__(self, name, healthQ, happyQ):
+    def __init__(self, name, ):
         self.name = name
+        self.foods = []
+        
+    def add_food(self, food):
+        self.foods.append(food)
+        return self
+    
+    def __repr__(self):
+        return f"{self.name}"
+
+class Foods(Food):
+    def __init__(self, name, healthQ, happyQ):
+        super().__init__(name)
         self.healthQ = healthQ
         self.happyQ = happyQ
-
-class Marines(Animal): #not tested
-    def __init__(self, name, nickname, size, age, health, happyness):
+        
+class Water(Animal): #working
+    def __init__(self, name, nickname, size, age, health=50, happyness=50):
         super().__init__(name, nickname, size, age, health, happyness)
         self.swimming = True
     
@@ -66,6 +97,7 @@ class Marines(Animal): #not tested
         if self.swimming == True:
             self.distance = distance
             self.health -= distance
+            self.happyness += distance
             if self.health <= 0:
                 self.health = 0
                 return (f"{self.nickname} has swam {self.distance} kms, and now is totally exhausted. His health is {self.health}%")
@@ -75,8 +107,8 @@ class Marines(Animal): #not tested
                 return (f"{self.nickname} has swam {self.distance} kms, but is so strong. His health is {self.health}%")
         return self
 
-class Earthlys(Animal):
-    def __init__(self, name, nickname, size, age, health, happyness):
+class Land(Animal):
+    def __init__(self, name, nickname, size, age, health=50, happyness=50):
         super().__init__(name, nickname, size, age, health, happyness)
         self.walking = True
     
@@ -84,6 +116,7 @@ class Earthlys(Animal):
         if self.walking == True:
             self.distance = distance
             self.health -= distance
+            self.happyness += distance
             if self.health <= 0:
                 self.health = 0
                 return (f"{self.nickname} has walked {self.distance} kms, and now is totally exhausted. His health is {self.health}%")
@@ -91,11 +124,9 @@ class Earthlys(Animal):
                 return (f"{self.nickname} has walked {self.distance} kms, and now is almost exhausted. His health is {self.health}% ")
             elif self.health >50:
                 return (f"{self.nickname} has walked {self.distance} kms, but is so strong. His health is {self.health}%")
-        else:
-            return (f"I'm afraid this animal can't walk")
 
-class Aerials(Animal): #not tested
-    def __init__(self, name, nickname, size, age, health, happyness):
+class Sky(Animal): #not tested
+    def __init__(self, name, nickname, size, age, health=50, happyness=50):
         super().__init__(name, nickname, size, age, health, happyness)
         self.flying = True
     
@@ -103,6 +134,7 @@ class Aerials(Animal): #not tested
         if self.flying == True:
             self.distance = distance
             self.health -= distance
+            self.happyness += distance
             if self.health <= 0:
                 self.health = 0
                 return (f"{self.nickname} has flew {self.distance} kms, and now is totally exhausted. His health is {self.health}%")
@@ -110,5 +142,4 @@ class Aerials(Animal): #not tested
                 return (f"{self.nickname} has flew {self.distance} kms, and now is almost exhausted. His health is {self.health}% ")
             elif self.health >50:
                 return (f"{self.nickname} has flew {self.distance} kms, but is so strong. His health is {self.health}%")
-        else:
-            return (f"I'm afraid this animal can't fly")
+            
